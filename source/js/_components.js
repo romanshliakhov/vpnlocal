5b8834f774fc47b7dd9f267f8b16ebdfb6dca6b7
Roman <romanshliakhov@gmail.com>
2022-11-17 16:53:25 +0200

    start devices section

--- source/js/_components.js
+++ source/js/_components.js
@@ -6,6 +6,7 @@ import './components/modals.js';
 import './components/smoothScroll.js';
 import './components/replaceEl.js';
 import './components/phoneMask.js';
+import './components/tabs.js';
 
 
 
