b70459dac960008ab02601079e0f14f3c600d216
Roman <romanshliakhov@gmail.com>
2022-11-20 23:36:28 +0200

    add vertical slider

--- source/js/components/sliders.js
+++ source/js/components/sliders.js
@@ -1,49 +1,20 @@
 import Swiper from '../vendor/swiper.js';
 
 for (const container of document.querySelectorAll('.container') ) {
-  let matSlider = new Swiper(container.querySelector('.mat__slider'), {
+  let titleSlider = new Swiper(container.querySelector('.hero__title-slider'), {
     slidesPerView: 1,
     loop: true,
-    observer: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+    autoplay: {
+      delay: 2500,
     },
-    navigation: {
-      nextEl: container.querySelector(".mat__slider-btn--next"),
-      prevEl: container.querySelector(".mat__slider-btn--prev"),
-      clickable: true,
-    },
+    direction: 'vertical',
+    autoHeight: true,
+    // cleanStyles: false,
+    // deleteInstance: false,
+    // effect: 'fade',
   });
 };
 
-let giftsSlider = new Swiper(".gifts__slider", {
-  slidesPerView: 1.75,
-  spaceBetween: 12,
-  centeredSlides: true,
-  loop: true,
-  observer: true,
-  breakpoints: {
-    320: {
-      slidesPerView: 1.75,
-      spaceBetween: 12,
-      },
-    768: {
-      slidesPerView: 2.75,
-      spaceBetween: 15,
-      centeredSlides: true,
-      },
-    1024: {
-      slidesPerView: 'auto',
-      // slidesPerView: 4,
-      spaceBetween: 20,
-      // adaptiveHeight: true,
-      centeredSlides: false,
-      loop: true,
-      observer: true,
-    }
-  }
-});
 
 
 
