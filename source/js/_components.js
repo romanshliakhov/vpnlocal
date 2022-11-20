73ef988c13277a7e066dd6ef407039ed15dae0bf
Roman <romanshliakhov@gmail.com>
2022-11-20 11:59:40 +0200

    add modals

--- source/js/_components.js
+++ source/js/_components.js
@@ -1,11 +1,11 @@
 import './components/sliders.js';
 import './components/select.js';
 import './components/burger.js';
-import './components/spollers.js';
+import './components/accordion.js';
 import './components/modals.js';
-import './components/smoothScroll.js';
+// import './components/smoothScroll.js';
 import './components/replaceEl.js';
-import './components/phoneMask.js';
+// import './components/phoneMask.js';
 import './components/tabs.js';
 import './components/animateplanet.js';
 
